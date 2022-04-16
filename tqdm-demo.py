b65e0eaa6702018397c30abdce70d1abb2112450
Dominic Kwok <61585411+kwoksir@users.noreply.github.com>
2022-04-16 22:22:34 +0800

    Update tqdm-demo.py

--- tqdm-demo.py
+++ tqdm-demo.py
@@ -1,7 +1,9 @@
 import re
-
+from tqdm import trange
+import time
 nameList = ["Chan Tai Man", "Lee Lap Man","Cheung Kwok Keung","Li Ho Yan"]
 R = []
-for x in nameList:
-    R.append(re.sub(r'[a-z]','*', x))
+for x in trange(len(nameList)):
+    R.append(re.sub(r'[a-z]','*', nameList[x]))
+    time.sleep(0.5)
 print(R)
